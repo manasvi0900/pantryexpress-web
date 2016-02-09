ce1c45dfa7b0cb09950a9249f6e82ac1534dd573
Mark <maschuerman1@gmail.com>
2016-02-09 13:22:28 -0800

    linting

--- test/spec/controllers/service.js
+++ test/spec/controllers/service.js
@@ -16,8 +16,4 @@ describe('Controller: ServiceCtrl', function () {
       // place here mocked dependencies
     });
   }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    //expect(ServiceCtrl.awesomeThings.length).toBe(3);
-  });
 });
