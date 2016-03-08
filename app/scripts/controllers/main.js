564e249eff7c7e097ca6118bff04c7feb986cf1d
Justin Pirtle <justonian@users.noreply.github.com>
2016-03-08 07:43:22 +0000

    Adding Site-wide Pantry Selection to Website

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('pantyexpressApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
