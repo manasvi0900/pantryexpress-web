c15b24ed2946c9493731139b534797ac10286361
Mark <maschuerman1@gmail.com>
2016-05-02 15:41:43 -0700

    removing call to selenium directly

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -25,7 +25,7 @@ exports.config = {
   // tests to work.
   // seleniumAddress: 'http://selenium.example.com:4444/wd/hub',
 
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  // seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   // -----------------------------------------------------------------
   // Specify the test code that will run.
   // -----------------------------------------------------------------
